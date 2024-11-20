a74bf201336e0eefe6a4149a6bb3a50da2fa231f
gabrieldario66@gmail.com <gabrieldario66@gmail.com>
2024-11-19 23:53:31 -0300

    Escutador de eventos js e form

---
+++ JavaScript para Web/introducao-javascript/js/principal.js
@@ -0,0 +1,6 @@
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+console.log(botaoAdicionar)
+botaoAdicionar.addEventListener("click", ()=> {
+    console.log("Oi, cliquei no botão.");
+}
+);
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Aprendizado na alura
 -------------------
 Relatório de estudos diários
+18-11-24 - 4:21
+19-11-24 - 12:52 - Formulário, erro  ->escutador de botão dentro do form atualiza página...
