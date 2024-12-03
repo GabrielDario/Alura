19c68dda5819a4affe5bb07c716f724462f2d19c
gabrieldario66@gmail.com <gabrieldario66@gmail.com>
2024-12-02 21:26:00 -0300

    React-iniciado e componentes

--- README.md
+++ README.md
@@ -3,5 +3,8 @@ Aprendizado na alura
 Relatório de estudos diários
 18-11-24 = 4:21
 19-11-24 = 12:52 - Formulário, erro  ->escutador de botão dentro do form atualiza página...
-28-11-24 = 3:00 ,16 minutos, 4:21, 4:48,10:23,16:04 TOTAL 43,73 - Começar CAP 6
+28-11-24 = 3:00 ,16 minutos, 4:21, 4:48,10:23,16:04 TOTAL 43,73 - Começar CAP 6 JAVASCRIPT
+
+02/12- Introdução React - 30:44,38,26,3:22,11:18,6:07,13:39, - 
+Como instalar,baixar,oque vamos fazer (Tarefas),Componetes
 
---
+++ React/ceep/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ListaDeNotas from "./components/ListaDeNotas" 
+
+function App() {
+    return (
+        <section>
+            <form>
+                <input type="text" placeholder="Título" />
+                <textarea placeholder="Escreva sua nota..."/> 
+                    <button>Criar Nota</button>
+            </form>
+            <ListaDeNotas />
+        </section>
+    );
+}
+//react-> lib
+//React -> ecossistema
+export default App;
