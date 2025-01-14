d987a3c0b5fed1c64429e7d40d7f4c843a3ecda3
gabrieldario66@gmail.com <gabrieldario66@gmail.com>
2025-01-13 21:27:10 -0300

    Bug?

--- React/organo/src/componentes/Time/index.js
+++ React/organo/src/componentes/Time/index.js
@@ -1,24 +1,26 @@
-import Colaborador from '../Colaborador'
 import './Time.css'
+import Colaborador from '../Colaborador';
+
 
 const Time = (props) => {
-    const css = { backgroundColor: props.corSecundaria }
-
+    const css = {
+        backgroundColor: props.corSecundaria
+    }
     return (
         (props.colaboradores.length > 0) ?
-         <section className='time' style={css}>
+        <section className='time' style={css}>
             <h3 style={{ borderColor: props.corPrimaria }}>
                 {props.nome}</h3>
             <div className='colaboradores'>
                 {props.colaboradores.map(
-                     colaborador => <Colaborador 
-                     key={colaborador.nome} 
-                     cargo={colaborador.cargo} 
-                     imagem={colaborador.imagem}/> )}
+                    colaborador =>  <Colaborador
+                    key={colaborador.nome}
+                    cargo={colaborador.cargo} 
+                    imagem={colaborador.imagem}/> )}
             </div>
-        </section> 
+        </section>
         : ''
     )
 }
 
-export default Time
+export default Time;
